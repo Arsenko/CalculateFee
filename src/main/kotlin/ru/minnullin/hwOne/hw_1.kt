4d25a8a3ee3c944e1c61dcc72abc6acb420080a5
Arsenko <minsenka@gmail.com>
2020-04-23 00:06:05 +0300

    work without errors, hope

--- src/main/kotlin/ru/minnullin/hwOne/hw_1.kt
+++ src/main/kotlin/ru/minnullin/hwOne/hw_1.kt
@@ -2,25 +2,42 @@ package ru.minnullin.hwOne
 
 fun main() {
     //стоимость продаж
-    val amount: Float = 100f
-    val total: Float = 1001f
-    val exclusive:Boolean=true
-    val fee:Float= CalculeteFee(amount, total, exclusive)
-    print("Сумма за вычетом налога = ${amount-fee}.Ваш налог составляет $fee");
+    //пример <1000
+    var amount = 100f
+    var total = 21f
+    var fee: Float = calculateFee(amount, total, true)
+    println("Сумма за вычетом налога = ${amount - fee}.Ваш налог составляет $fee");
+    fee = calculateFee(amount, total, false)
+    println("Сумма за вычетом налога = ${amount - fee}.Ваш налог составляет $fee");
+    //пример >1000
+    amount = 100f
+    total = 1001f
+    fee = calculateFee(amount, total, true)
+    println("Сумма за вычетом налога = ${amount - fee}.Ваш налог составляет $fee");
+    fee = calculateFee(amount, total, false)
+    println("Сумма за вычетом налога = ${amount - fee}.Ваш налог составляет $fee");
+    //пример >10000
+    amount = 200f
+    total = 10001f
+    fee = calculateFee(amount, total, true)
+    println("Сумма за вычетом налога = ${amount - fee}.Ваш налог составляет $fee");
+    fee = calculateFee(amount, total, false)
+    println("Сумма за вычетом налога = ${amount - fee}.Ваш налог составляет $fee");
+    //пример >50000
+    amount = 300f
+    total = 50001f
+    fee = calculateFee(amount, total, true)
+    println("Сумма за вычетом налога = ${amount - fee}.Ваш налог составляет $fee");
+    fee = calculateFee(amount, total, false)
+    println("Сумма за вычетом налога = ${amount - fee}.Ваш налог составляет $fee");
 }
 
-fun CalculeteFee(amount: Float, total: Float, exclusive: Boolean): Float {
-    val exclusiveSale:Float=if(exclusive){
-        0.05f
-    }else
-        0f
+fun calculateFee(amount: Float, total: Float, exclusive: Boolean): Float {
+    val exclusiveSale: Float = if (exclusive) 0.05f else 0f
     return (when {
-        total > 50000 ->
-            amount * (0.15 - exclusiveSale)
-        total > 10000 ->
-            amount * (0.2 - exclusiveSale)
-        total > 1000 ->
-            amount * (0.25 - exclusiveSale)
-        else -> amount * 0.30
+        total > 50000 -> amount * (0.15 - exclusiveSale)
+        total > 10000 -> amount * (0.2 - exclusiveSale)
+        total > 1000 -> amount * (0.25 - exclusiveSale)
+        else -> amount * (0.30 - exclusiveSale)
     }).toFloat()
 }
